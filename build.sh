1dc390dacbe2eb747099e8c1934f1b7bc019be53
weaponmasterjax <42597871+weaponmasterjax@users.noreply.github.com>
2025-01-16 21:58:24 +0800

    defconfig: bump version code

--- build.sh
+++ build.sh
@@ -22,7 +22,7 @@ CURRENT_DIR=$(pwd)
 # Device Configs
 DEVICE="everpal"
 DEFCONFIG="${DEVICE}_defconfig"
-ZIPNAME="AdrenalinKernel-${DEVICE}-${DATE}.zip"
+ZIPNAME="AdrenalinKernel-V2-${DEVICE}-${DATE}.zip"
 
 # Ensure the toolchain is available
 if [ ! -d "$TC_DIR" ]; then
