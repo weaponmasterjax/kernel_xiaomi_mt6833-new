3848d25af96e7e86f7fc50abc1a087c68a97dc01
weaponmasterjax <42597871+weaponmasterjax@users.noreply.github.com>
2025-01-23 13:38:40 +0800

    sched/fair: Backport reweight_task
    cherry picked from "sched/fair: Use reweight_entity() for set_user_nice()"

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -2900,7 +2900,6 @@ void reweight_task(struct task_struct *p, int prio)
 	struct cfs_rq *cfs_rq = cfs_rq_of(se);
 	struct load_weight *load = &se->load;
 	unsigned long weight = scale_load(sched_prio_to_weight[prio]);
-
 	reweight_entity(cfs_rq, se, weight);
 	load->inv_weight = sched_prio_to_wmult[prio];
 }
