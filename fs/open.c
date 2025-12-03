b4acdc57d5d4fe50fb5b5f49f049e8f49e9b560c
weaponmasterjax <42597871+weaponmasterjax@users.noreply.github.com>
2025-12-03 21:17:14 +0800

    apply scope_min_manual_hooks v1.4
    Signed-off-by: weaponmasterjax <42597871+weaponmasterjax@users.noreply.github.com>

--- fs/open.c
+++ fs/open.c
@@ -359,8 +359,16 @@ SYSCALL_DEFINE4(fallocate, int, fd, int, mode, loff_t, offset, loff_t, len)
  * We do this by temporarily clearing all FS-related capabilities and
  * switching the fsuid/fsgid around to the real ones.
  */
+#if defined(CONFIG_KSU) && !defined(CONFIG_KSU_KPROBES_HOOK)
+extern __attribute__((hot, always_inline)) int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,
+			                    int *flags);
+#endif
+
 SYSCALL_DEFINE3(faccessat, int, dfd, const char __user *, filename, int, mode)
 {
+#if defined(CONFIG_KSU) && !defined(CONFIG_KSU_KPROBES_HOOK)
+	ksu_handle_faccessat(&dfd, &filename, &mode, NULL);
+#endif
 	const struct cred *old_cred;
 	struct cred *override_cred;
 	struct path path;
